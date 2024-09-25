6e9838d7f6e21fd28a15d41b3f7b512d92e5c6fa
A C <72420910+acphil2@users.noreply.github.com>
2024-09-25 15:02:03 -0700

    animations, fixed images, middle row content

--- src/components/Middlerow/Middlerow.js
+++ src/components/Middlerow/Middlerow.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import './Middlerow.css';
-import gameImage from '../../Assets/images/game-ui.png';
+import gameUiImage from '../../Assets/images/game-ui.png';
+import gameLogo from '../../Assets/images/holly-haunted-logo-light.png';
+import hollyCharacter from '../../Assets/images/holly.PNG';
 
 
-const Middlerow = ( props ) => {  
-    return(
-    	<div className='middle-row row'>
-    	    <div className='middle-left col-md-4'>
-                <div className='holly-ui-img'>
-                    <img src={gameImage} className='img-fluid' />
+const Middlerow = (props) => {
+    return (
+        <div className='middle-row row'>
+
+            <div className='middle-left col-lg-6 col-md-12'>
+                <img src={hollyCharacter} className='holly-img' alt="Holly Haunted Character" />
+                <div className='holly-logo-container'>
+                    <img src={gameLogo} className='holly-logo-img' alt="Holly Haunted Game UI" />
                 </div>
-            </div>	
-    		<div className='middle-right col-md-8'>
-    			<a href='https://acphil2.github.io/game/' target='_blank' rel='noreferrer'>
-                    <div className='holly'>
-    				    <h2>Holly Haunted</h2>
-    			    </div>
-                </a>
-                <div className='circle'>
-                        <a href='https://acphil2.github.io/game/' target='_blank' rel='noreferrer'>
-                            <div className='holly-image'></div>
-                        </a>
-                    <div className='paragraph'>
-                        <i>Holly Haunted</i><br/> is a game made with React
-                    </div>
+                <p>
+                    Put out all of the candles before the ghosts can relight them while exploring a haunted Victorian mansion. Coded in JavaScript, <i>Holly Haunted</i> is an original RPG game built with React. Use keyboard arrows to navigate and spacebar to blow out candles.
+                </p>
+                <div>
+                    <div type="button" class="btn btn-outline-primary">PLAY NOW</div>
                 </div>
-    		</div>	
-	    </div>
-    ); 
+            </div>
+            <div className='middle-right col-lg-6 col-md-12'>
+                <div className='holly-ui-img-container'>
+                    <img src={gameUiImage} className='img-fluid holly-ui-img' alt="Holly Haunted Game UI" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Middlerow;

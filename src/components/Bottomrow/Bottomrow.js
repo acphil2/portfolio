793d612f62a3a2c0607935aa0f7945943a1c504b
A C <72420910+acphil2@users.noreply.github.com>
2024-09-26 10:53:35 -0700

    added tooltips to icons

--- src/components/Bottomrow/Bottomrow.js
+++ src/components/Bottomrow/Bottomrow.js
@@ -3,12 +3,12 @@ import './Bottomrow.css'
 
 const Bottomrow = (props) => {
     return (
-        <div className='bottom-row row'>
+        <div className='bottom-row row' id="litebrite">
             <div className='bottom-left col-lg-6 col-md-12'>
                 <a href='https://acphil2.github.io/light-board/' target='_blank' rel='noreferrer'>
                     <h2>LITE•BRITE</h2>
                 </a>
-                <p>Inspired by the iconic light box toy, this project was programmed with JavaScript and built with React. By clicking individual lightbulbs and using the palette to change colors, users can illuminate the board to create custom designs.</p>
+                <p>Inspired by the iconic light box toy, this project was programmed in JavaScript and built with React. By clicking lightbulbs and the color palette, users can illuminate the board to create custom designs.</p>
                 <a href='https://acphil2.github.io/light-board/' target='_blank' rel='noreferrer'>
                     <div type="button" class="btn btn-outline-primary">PLAY NOW</div>
                 </a>
